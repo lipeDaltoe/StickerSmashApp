d7455e35daa789fae40cfd9d92555bd4430e59d2
lipeDaltoe <felipedaltoe9@gmail.com>
2025-03-20 11:36:07 -0300

    Aidicionando imagem da galeria (e a meditação hein?)

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
 import * as ImagePicker from 'expo-image-picker';
 
+
 const PlaceholderImage = require('@/assets/images/background-image.png')
 
 export default function Index() {
+
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+  const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ['images'],
@@ -14,7 +20,8 @@ export default function Index() {
     });
 
     if (!result.canceled) {
-      console.log(result);
+      setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
     } else {
       alert("Você não escolheu uma imagem!!!");
     }
@@ -23,18 +30,23 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
-      <View style={styles.footerContainer}>
-        <Button 
-        label="Escolher uma Foto" 
-        theme="primary" 
-        onPress={pickImageAsync} 
-        />
-        <Button label="Usar esta Foto" />
-      </View>
-    </View>
-  );
+      {showAppOptions ? (
+        <View>
+        </View>
+      ) : (
+        <View style = {styles.footerContainer}>
+          <Button
+            label="Escolher uma Foto"
+            theme="primary"
+            onPress={pickImageAsync}
+          />
+          <Button label="Usar esta Foto" onPress={() => setShowAppOptions(true)}/>
+        </View>
+  )} 
+  </View >
+);
 }
 
 const styles = StyleSheet.create({
@@ -47,7 +59,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   footerContainer: {
-    flex: 1/3,
+    flex: 1 / 3,
     alignItems: 'center',
   }
 })
